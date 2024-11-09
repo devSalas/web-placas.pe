8be8acb72d8db331255e409f5f0232263e4a585e
estaban salas sulca <esalassulca@gmail.com>
2024-11-09 18:58:37 -0500

    feat: crear componente navbarReglamentos

--- src/data/components/base_legal/reglamentos.ts
+++ src/data/components/base_legal/reglamentos.ts
@@ -1,51 +1,58 @@
 export const dataReglamentoTitulos = {
-    titulos: [
-        {
-            nombre: "TITULO PRELIMINAR",
-            descripcion: "DISPOSICIONES GENERALES"
-        },
-        {
-            nombre: "TITULO I",
-            descripcion: "CLASIFICACIÓN DE LA PLACA ÚNICA NACIONAL DE RODAJE"
-        },
-        {
-            nombre: "TITULO II",
-            descripcion: "SISTEMA DE CODIFICACIÓN DE LA PLACA ÚNICA NACIONAL DE RODAJE"
-        },
-        {
-            nombre: "TITULO III",
-            descripcion: "ESPECIFICACIONES TÉCNICAS DE LA PLACA ÚNICA NACIONAL DE RODAJE Y DE LA TARJETA DE IDENTIFICACIÓN VEHICULAR"
-        },
-        {
-            nombre: "TITULO IV",
-            descripcion: "PROCEDIMIENTO DE ASIGNACIÓN DE LA PLACA ÚNICA NACIONAL DE RODAJE"
-        },
-        {
-            nombre: "TITULO V",
-            descripcion: "CAMBIO, INVALIDEZ Y CADUCIDAD DE LA PLACA ÚNICA NACIONAL DE RODAJE"
-        }
-       
-    ],
-    disposicion:[
-        {
-            nombre: "DISPOSICIONES COMPLEMENTARIAS",
-            descripcion: ""
-        },
+    titulos: {
+        title: "TITULO PRELIMINAR",
+        subtitle: "DISPOSICIONES GENERALES",
+        content: [
+            {
+                nombre: "TITULO I",
+                descripcion: "CLASIFICACIÓN DE LA PLACA ÚNICA NACIONAL DE RODAJE",
+                enlace: ""
+            },
+            {
+                nombre: "TITULO II",
+                descripcion: "SISTEMA DE CODIFICACIÓN DE LA PLACA ÚNICA NACIONAL DE RODAJE",
+                enlace: ""
+            },
+            {
+                nombre: "TITULO III",
+                descripcion: "ESPECIFICACIONES TÉCNICAS DE LA PLACA ÚNICA NACIONAL DE RODAJE Y DE LA TARJETA DE IDENTIFICACIÓN VEHICULAR",
+                enlace: ""
+            },
+            {
+                nombre: "TITULO IV",
+                descripcion: "PROCEDIMIENTO DE ASIGNACIÓN DE LA PLACA ÚNICA NACIONAL DE RODAJE",
+                enlace: ""
+            },
+            {
+                nombre: "TITULO V",
+                descripcion: "CAMBIO, INVALIDEZ Y CADUCIDAD DE LA PLACA ÚNICA NACIONAL DE RODAJE",
+                enlace: ""
+            }]
+
+    },
+    disposicion: {
+        title: "DISPOSICIONES COMPLEMENTARIAS",
+        subtitle: "",
+        content: [
         {
             nombre: "CRONOGRAMA PARA EL CAMBIO DE PLACAS – VEHICULOS DE SERVICIO DE TRANSPORTE DE PERSONAS Y TRANSPORTE DE MERCANCIAS",
-            descripcion: ""
+            descripcion: "",
+            enlace: ""
         },
         {
             nombre: "DISPOSICIONES COMPLEMENTARIAS FINALES",
-            descripcion: ""
+            descripcion: "",
+            enlace: ""
         },
         {
             nombre: "DISPOSICIONES COMPLEMENTARIAS TRANSITORIAS",
-            descripcion: ""
+            descripcion: "",
+            enlace: ""
         },
         {
             nombre: "DISPOSICIONES COMPLEMENTARIAS MODIFICATORIAS",
-            descripcion: ""
-        }
-    ]
+            descripcion: "",
+            enlace: ""
+        }]
+    }
 }
